36df231adb34f390c8bb5e1afd26fa47c6041094
Emilio <emiliogarb@hotmail.com>
2025-05-14 10:55:50 -0600

    Update Commit

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+#python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
